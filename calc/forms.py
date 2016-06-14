d3a5d595e331210e344f447c9126484fa86e54bc
dhcrain <dhcrain@gmail.com>
2016-06-14 18:03:32 -0400

    Cleaned up

--- calc/forms.py
+++ calc/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class CalcForm(forms.Form):
-    operator_choice = [('+', '+'), ('-', '-'), ('/', '/'), ('*', 'X')]
+    operator_choice = [('+', '+'), ('-', '-'), ('/', '/'), ('X', 'X')]
     num1 = forms.FloatField()
     operator = forms.ChoiceField(operator_choice)
     num2 = forms.FloatField()
--- calc/views.py
+++ calc/views.py
@@ -4,37 +4,31 @@ from calc.forms import CalcForm
 
 
 def index_view(request):
+    result = 0
+    num1 = 0
+    num2 = 0
+    operator = ""
     if request.POST:
         form = CalcForm(request.POST)
         if form.is_valid():
             num1 = form.cleaned_data['num1']
             num2 = form.cleaned_data['num2']
             operator = form.cleaned_data['operator']
-            print(num1, operator, num2)
             if operator == '+':
                 result = num1 + num2
-                next_math = CalcForm(initial={"num1": result})
-                return render(request, 'index.html',
-                              {"form": CalcForm(),
-                               'result': result,
-                               'equation': [num1, operator, num2],
-                               'next_math': next_math})
             elif operator == '-':
                 result = num1 - num2
-                return render(request, 'index.html',
-                              {"form": CalcForm(),
-                               'result': result,
-                               'equation': [num1, operator, num2]})
             elif operator == '/':
                 result = num1 / num2
-                return render(request, 'index.html',
-                              {"form": CalcForm(),
-                               'result': result,
-                               'equation': [num1, operator, num2]})
-            elif operator == '*':
+            elif operator == 'X':
                 result = num1 * num2
-                return render(request, 'index.html',
-                              {"form": CalcForm(),
-                               'result': result,
-                               'equation': [num1, 'X', num2]})
-    return render(request, 'index.html', {"form": CalcForm()})
+    next_math = CalcForm(initial={"num1": result})
+    return render(request, 'index.html',
+                  {"form": CalcForm(),
+                   'result': result,
+                   'num1': num1,
+                   'num2': num2,
+                   'operator': operator,
+                   'next_math': next_math
+                   })
+    # return render(request, 'index.html', {"form": CalcForm()})
