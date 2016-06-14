b85408f251f05c9b8322c475ac61f704e7d9f028
dhcrain <dhcrain@gmail.com>
2016-06-14 15:50:04 -0400

    plus works

--- calc/views.py
+++ calc/views.py
@@ -7,7 +7,8 @@ def index_view(request):
     if request.POST:
         form = CalcForm(request.POST)
         if form.is_valid():
-            if form.operator == '+':
-                result = form.num1 + form.num2
-                return result
+            print(form.cleaned_data['operator'])
+            # if form.cleaned_data['operator'] == '+':
+            result = form.cleaned_data['num1'], form.cleaned_data['operator'], form.cleaned_data['num2']
+            return render(request, 'index.html', {"form": CalcForm(), 'result': result})
     return render(request, 'index.html', {"form": CalcForm()})
